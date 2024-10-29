bc0563aaff71aa1d916bd3067cf4ae2af1f53069
khv2644511 <khv2644511@naver.com>
2024-10-29 14:35:21 +0900

    지정한 선사의 운임 api로 받아오기

--- packet.py
+++ packet.py
@@ -83,8 +83,6 @@ class FreightAutomation:
             self.driver.get("https://www.ekmtc.com/index.html#/schedule/leg")
             time.sleep(5)
 
-          # Incorrect method causing "unhashable type: 'dict'" error
-            # Correct usage
             def interceptor(request):
                 token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzZXNzSWQiOiJBZDBpRVBwZ19WY0tkTUhxcXdLaVFmUEZRMENFQWJsaGtEaUZwNmhyLTEzNDU0OCIsInVzZXJJZCI6IllPTDAwM18wMDEiLCJvcmdVc2VySWQiOiJZT0xDQVJHTyIsInVzZXJOYW1lIjoi6rCV7J246recIiwidXNlckVuYW1lIjoiWU9MIElOQyIsImNzdENhdENkIjoiMDEiLCJ1c2VyVHlwZSI6IjkiLCJjc3RDZCI6IllPTDAwMyIsInN0YWZmRmxhZyI6Ik4iLCJ1c2VyQ3RyQ2QiOiJLUiIsImFkbWluIjpmYWxzZSwicm9sZSI6Ik1FTUJFUiIsInNlcnZpY2VMYW5nIjoiS09SIiwiaWF0IjoxNzI5NzQ1MTQ4LCJleHAiOjE3MzI0MjM1NDh9.JACVS2Ki0YukSEM3QCwgutyESKNrK6mK9ww5yEb8w2I'
                 request.headers['Authorization'] = f'Bearer {token}'
@@ -93,96 +91,126 @@ class FreightAutomation:
             # headers = {
             #     'Authorization': f'Bearer {token}'  # Ensure the token is prefixed with 'Bearer' if required by the API
             # }
+            param = {
+                "startPlcCd": "PUS",
+                "searchMonth": "11",
+                "bound": "O",
+                "startPlcName": "Busan, Korea (PUS)",
+                "destPlcCd": "HKG",
+                "searchYear": "2024",
+                "filterYn": "N",
+                "searchYN": "Y",
+                "startCtrCd": "KR",
+                "destCtrCd": "HK",
+                "destPlcName": "Hong Kong (HKG)",
+                "main": "N",
+                "legIdx": "0",
+                "vslType01": "01",
+                "vslType03": "03",
+                "eiCatCd": "O",
+                "calendarOrList": "C",
+                "cpYn": "N",
+                "promotionChk": "N",
+
+            }
+            
             self.driver.get('https://api.ekmtc.com/schedule/schedule/leg/search-schedule?startPlcCd=PUS&searchMonth=11&pointChangeYN=&bound=O&filterPolCd=&pointLength=&startPlcName=Busan,+Korea+(PUS)&destPlcCd=HKG&searchYear=2024&filterYn=N&searchYN=Y&filterPodCd=&hiddestPlcCd=&startCtrCd=KR&destCtrCd=HK&polTrmlStr=&podTrmlStr=&rteCd=&filterTs=Y&filterDirect=Y&filterTranMax=0&filterTranMin=0&hidstartPlcCd=&destPlcName=Hong+Kong+(HKG)&main=N&legIdx=0&vslType01=01&vslType03=03&unno=&commodityCd=&eiCatCd=O&calendarOrList=C&cpYn=N&promotionChk=N&vslCd=&voyNo=')
+            # requests.get('https://api.ekmtc.com/schedule/schedule/leg/search-schedule', params=param)
+            time.sleep(5)
+
+            # 스케줄 검색 응답 결과 저장
             elements = self.driver.find_element(By.XPATH, '/html/body/pre')
-            print(elements.text)
+            # print(elements.text)
 
             json_data = json.loads(elements.text)
             time.sleep(5)
             with open('schedule.json', 'w') as f:
                 json.dump(json_data, f)
-            # request = self.driver.wait_for_request('https://api.ekmtc.com/schedule/schedule/leg/search-schedule?startPlcCd=PUS&searchMonth=11&pointChangeYN=&bound=O&filterPolCd=&pointLength=&startPlcName=Busan,+Korea+(PUS)&destPlcCd=HKG&searchYear=2024&filterYn=N&searchYN=Y&filterPodCd=&hiddestPlcCd=&startCtrCd=KR&destCtrCd=HK&polTrmlStr=&podTrmlStr=&rteCd=&filterTs=Y&filterDirect=Y&filterTranMax=0&filterTranMin=0&hidstartPlcCd=&destPlcName=Hong+Kong+(HKG)&main=N&legIdx=0&vslType01=01&vslType03=03&unno=&commodityCd=&eiCatCd=O&calendarOrList=C&cpYn=N&promotionChk=N&vslCd=&voyNo=')
-            # print(request.response)
-            # content_type = request.response.headers.get('Content-Type')
-            # print(request.response.resstatus_code)
-            # print(request.response.headers.get('Content-Type'))
-            # if 'text' in content_type or 'json' in content_type:
-                
-            # self.driver.get(self.schedule_url)
-            # print(res_schedule.text.find(By.CSS_SELECTOR, '#autocomplete-form-input'))
-            # time.sleep(5)
-            # # 구간 선택
-            # departPort = self.driver.find_element(By.CSS_SELECTOR, '#autocomplete-form-input')
-            # departPort.send_keys('Busan, Korea (PUS)')
-            
-            # WebDriverWait(self.driver, 10).until(
-            # EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div/div[2]/div[1]/div/div[1]/form/div[1]/table/tbody/tr[1]/td[1]/div/div[2]/button[1]")))
 
-            # self.driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div/div[2]/div[1]/div/div[1]/form/div[1]/table/tbody/tr[1]/td[1]/div/div[2]/button[1]').click()
-            # time.sleep(3)
-
-            # arrivePort = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div/div[2]/div[1]/div/div[1]/form/div[1]/table/tbody/tr[1]/td[2]/div/div[1]/input')
-            # arrivePort.send_keys('Hong Kong (HKG)')
-
-            # WebDriverWait(self.driver, 10).until(
-            # EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div/div[2]/div[1]/div/div[1]/form/div[1]/table/tbody/tr[1]/td[2]/div/div[2]/button[1]")))
-
-            # self.driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div/div[2]/div[1]/div/div[1]/form/div[1]/table/tbody/tr[1]/td[2]/div/div[2]/button[1]').click()
-
+           
             print("port selected")
 
+            time.sleep(5)
+            
+            # https://api.ekmtc.com/schedule/schedule/leg/pop-fre-app-no?porCtrCd=KR&porPlcCd=PUS&dlyCtrCd=HK&dlyPlcCd=HKG&eiCatCd=O&logYn=N&etd=20241122&promotionChk=N&vslCd=JSOR&voyNo=2415S&rteCd=KTS&hotDealYn=N&hotDealReqRno=&raTsParam=
+
+            self.driver.get('https://api.ekmtc.com/schedule/schedule/leg/pop-fre-app-no?porCtrCd=KR&porPlcCd=PUS&dlyCtrCd=HK&dlyPlcCd=HKG&eiCatCd=O&logYn=N&etd=20241112&promotionChk=N&vslCd=&voyNo=&rteCd=KTS&hotDealYn=N&hotDealReqRno=&raTsParam=')
+            getReqNo = self.driver.find_element(By.XPATH, '/html/body/pre')
+            # print(elements.text)
+
+            getReqNo_json_data = json.loads(getReqNo.text)
+
+            reqRno_value = getReqNo_json_data.get("reqRno")
+            print(reqRno_value)
+
+            print('success')
+            return reqRno_value
         except Exception as e:
             print(f"Error selecting set port: {str(e)}")
 
-    def set_next_month(self):
-        try: 
-            self.driver.find_element(By.CSS_SELECTOR, '#frmLeg > div:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(2) > span:nth-child(1) > div > div > input').click()
+    # def set_next_month(self):
+    #     try: 
+    #         self.driver.find_element(By.CSS_SELECTOR, '#frmLeg > div:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(2) > span:nth-child(1) > div > div > input').click()
 
 
-            WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.ID, "MonthPicker_")))
+    #         WebDriverWait(self.driver, 10).until(
+    #         EC.presence_of_element_located((By.ID, "MonthPicker_")))
 
-            time.sleep(4)
-            self.driver.find_element(By.CLASS_NAME, 'button-11').click()
-            time.sleep(3)
-            self.driver.find_element(By.CSS_SELECTOR, '#frmLeg > div.position_relative > span > a').click()
-            print("set_next_month successfully")
+    #         time.sleep(4)
+    #         self.driver.find_element(By.CLASS_NAME, 'button-11').click()
+    #         time.sleep(3)
+    #         self.driver.find_element(By.CSS_SELECTOR, '#frmLeg > div.position_relative > span > a').click()
+    #         print("set_next_month successfully")
 
-        except Exception as e:
-            print(f"Error set next month: {str(e)}")
+    #     except Exception as e:
+    #         print(f"Error set next month: {str(e)}")
    
 
-    def check_freight(self):
+    # def check_freight(self):
+    #     try:
+    #         # Implementation to select the specific date and check freight rates
+    #         self.driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div/div[2]/div[1]/div/div[2]/div/div[2]/table/tbody/tr[3]/td[3]/div/div/div[24]/div/div/p/a').click()
+    #         time.sleep(3)
+
+    #         # 운임확인 버튼 클릭
+    #         self.driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div/div[2]/div[1]/div/div[4]/div/a[1]').click()
+    #         print("Freight checked")
+
+    #     except Exception as e:
+    #         print(f"Error checking freight: {str(e)}")
+
+    def get_freight_data(self, reqRno_value):
+        # 선사 스케줄 클릭
         try:
-            # Implementation to select the specific date and check freight rates
-            self.driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div/div[2]/div[1]/div/div[2]/div/div[2]/table/tbody/tr[3]/td[3]/div/div/div[24]/div/div/p/a').click()
-            time.sleep(3)
+            # def interceptor(request):
+            #     token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzZXNzSWQiOiJBZDBpRVBwZ19WY0tkTUhxcXdLaVFmUEZRMENFQWJsaGtEaUZwNmhyLTEzNDU0OCIsInVzZXJJZCI6IllPTDAwM18wMDEiLCJvcmdVc2VySWQiOiJZT0xDQVJHTyIsInVzZXJOYW1lIjoi6rCV7J246recIiwidXNlckVuYW1lIjoiWU9MIElOQyIsImNzdENhdENkIjoiMDEiLCJ1c2VyVHlwZSI6IjkiLCJjc3RDZCI6IllPTDAwMyIsInN0YWZmRmxhZyI6Ik4iLCJ1c2VyQ3RyQ2QiOiJLUiIsImFkbWluIjpmYWxzZSwicm9sZSI6Ik1FTUJFUiIsInNlcnZpY2VMYW5nIjoiS09SIiwiaWF0IjoxNzI5NzQ1MTQ4LCJleHAiOjE3MzI0MjM1NDh9.JACVS2Ki0YukSEM3QCwgutyESKNrK6mK9ww5yEb8w2I'
+            #     request.headers['Authorization'] = f'Bearer {token}'
+            # # self.driver.get('https://api.ekmtc.com/schedule/schedule/leg/vsl-schedule-info-detail?bound=O&podPortCd=HKG&polPortCd=PUS&voyNo=2408S&vslCd=JSKC&polTrmlCd=HBGT')
 
-            # 운임확인 버튼 클릭
-            self.driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div/div[2]/div[1]/div/div[4]/div/a[1]').click()
-            print("Freight checked")
+            # # 운임 확인 api 요청 url
+            # self.driver.request_interceptor = interceptor
+            token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzZXNzSWQiOiJBZDBpRVBwZ19WY0tkTUhxcXdLaVFmUEZRMENFQWJsaGtEaUZwNmhyLTEzNDU0OCIsInVzZXJJZCI6IllPTDAwM18wMDEiLCJvcmdVc2VySWQiOiJZT0xDQVJHTyIsInVzZXJOYW1lIjoi6rCV7J246recIiwidXNlckVuYW1lIjoiWU9MIElOQyIsImNzdENhdENkIjoiMDEiLCJ1c2VyVHlwZSI6IjkiLCJjc3RDZCI6IllPTDAwMyIsInN0YWZmRmxhZyI6Ik4iLCJ1c2VyQ3RyQ2QiOiJLUiIsImFkbWluIjpmYWxzZSwicm9sZSI6Ik1FTUJFUiIsInNlcnZpY2VMYW5nIjoiS09SIiwiaWF0IjoxNzI5NzQ1MTQ4LCJleHAiOjE3MzI0MjM1NDh9.JACVS2Ki0YukSEM3QCwgutyESKNrK6mK9ww5yEb8w2I'
 
-        except Exception as e:
-            print(f"Error checking freight: {str(e)}")
+            # request.headers['Authorization'] = f'Bearer {token}'
+            header = {
+                'Authorization': f'Bearer {token}'
+            }
+            requests.get(f'https://api.ekmtc.com/schedule/schedule/leg/pop-fre-surcharge?porCtrCd=KR&porPlcCd=PUS&dlyCtrCd=HK&dlyPlcCd=HKG&polNm=BUSAN,KOREA&podNm=HONG+KONG&etd=20241115&frtAppNo=&vslCd=JSKC&vslNm=SKY+CHALLENGE&voyNo=2408', headers=header)
+            # self.driver.get(f'https://api.ekmtc.com/schedule/schedule/leg/pop-fre-surcharge?porCtrCd=KR&porPlcCd=PUS&dlyCtrCd=HK&dlyPlcCd=HKG&polNm=BUSAN,KOREA&podNm=HONG+KONG&etd=20241115&frtAppNo=&vslCd=JSKC&vslNm=SKY+CHALLENGE&voyNo=2408S&reqRno={reqRno_value}')
+            # request = self.driver.wait_for_request('/schedule/schedule/leg/pop-fre-surcharge/*', timeout=30)
+            # print('response =', response.status_code)
+           
+            time.sleep(5)
 
-    def get_freight_data(self):
-        self.driver.get('https://api.ekmtc.com')
-        try:
-            request = self.driver.wait_for_request('./schedule/schedule/leg/pop-fre-surcharge/*')
-            content_type = request.response.headers.get('Content-Type', '')
-            print(content_type)
-            if 'text' in content_type or 'json' in content_type:
-                json_data = json.loads(request.response.body.decode('utf-8'))
-                time.sleep(5)
-                with open('data2.json', 'w') as f:
-                    json.dump(json_data, f)
-            elif 'image' in content_type:
-                image_data = request.response.body
-                print(f'Image retrieved, 응답코드 {request.response.status_code}, 컨텐츠 유형: {content_type}')
-                with open('downloaded_image.png', 'wb') as f:
-                    f.write(image_data)
-            else:
-                print(f'Unhandled content type {content_type}, 응답코드 {request.response.status_code}')
+            # 스케줄 검색 응답 결과 저장
+            elements = self.driver.find_element(By.XPATH, '/html/body/pre')
+            # print(elements.text)
+
+            json_data = json.loads(elements.text)
+            time.sleep(5)
+            with open('freight_data.json', 'w') as f:
+                json.dump(json_data, f)
+
             print("get_freight_data successfully")
 
         except Exception as e:
@@ -204,13 +232,13 @@ if __name__ == "__main__":
     bot = FreightAutomation()
     bot.login()
     time.sleep(3)
-    bot.set_port()
+    reqRno_value = bot.set_port()
     time.sleep(2)
     # bot.set_next_month() 
     # time.sleep(3)
     # bot.check_freight()
     # time.sleep(3)
-    # bot.get_freight_data()
+    bot.get_freight_data(reqRno_value)
     # bot.check_freight()
     # bot.logout()
     # bot.close()
